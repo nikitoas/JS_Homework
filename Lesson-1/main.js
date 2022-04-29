54634e300ad8652b57b967aa05277edc8cf2d5c4
Mikita Aucharenka <nikitoas17.92@gmail.com>
2022-04-29 17:42:21 +0200

    Modimodified:main.js

--- Lesson-1/main.js
+++ Lesson-1/main.js
@@ -1,25 +1,18 @@
 //Homework 05.04.2022
 
-var a = 2;
-var b = 4;
-    
-console.log(a,b ,'- Checking the value of variables');
+//Способ 1
+var a = 17,
+    b = 3,
+    c;
 
-var a = 4;
-var b = 2;
+c = a; 
+a = b; 
+b = c; 
 
-console.log(a, ' - Variable value (a) changed ');
-console.log(b, ' - Variable value (b) changed');
+// Способ 2
+var a = 17,
+    b = 3;
 
-// 'copy data from one to another'
-
-// let a = 2;
-// let b = 4;
-    
-// console.log(a,b ,'- Checking the value of variables');
-
-// a = b;
-// b = 2;
-
-// console.log(a, ' - Variable value (a) changed ');
-console.log(b, ' - Variable value (b) changed');
+a += b; 
+b = a - b; 
+a -= b; 
